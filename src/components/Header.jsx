a72c3972c6fc19e9b51b8d4e8c0811f2f61df133
Jonathan Maimone <jonathanfmaimone@gmail.com>
2023-07-24 16:21:42 -0400

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ function Header() {
   const [collapsed, setCollapsed] = useState(true);
 
   return (
-    <Navbar color="faded" light>
+    <Navbar color="faded" dark>
       <NavbarBrand href="/" className="me-auto">
         <img style={{ height: "3rem" }} src={logo} />
       </NavbarBrand>
