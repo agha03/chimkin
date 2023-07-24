08610173f79179763527b635fba23e4d4bf37447
Adam Gaspar <adam.c.gaspar@gmail.com>
2023-07-24 16:09:14 -0400

    grass

--- src/pages/RateChickens.jsx
+++ src/pages/RateChickens.jsx
@@ -5,16 +5,12 @@ import { Alert, Button, Container, Row } from "reactstrap";
 import Header from "../components/Header";
 import LoadingSpinner from "../components/LoadingSpinner";
 import { downdootChicken, getAllChickens, updootChicken } from "../utils/api";
-import useSound from "use-sound";
-import wolfWhistle from "../assets/wolfWhistle.mp3"
 
 function RateChickens() {
   const [loading, setLoading] = useState(false);
   const [chickens, setChickens] = useState([]);
   const [index, setIndex] = useState(0);
   const [errorMsg, setErrorMsg] = useState(null);
-  const [playSound] = useSound(wolfWhistle)
-  
 
   const chicken = chickens[index];
 
@@ -30,7 +26,6 @@ function RateChickens() {
   useEffect(getNewChicken, []);
 
   function handleUpdoot() {
-    playSound();
     setErrorMsg(null);
     setLoading(true);
     updootChicken(chicken.id)
@@ -56,15 +51,15 @@ function RateChickens() {
       {!loading && !errorMsg && index <= chickens.length - 1 ? (
         <Container>
           <Row>
-            <div className="p-2 bg-light d-flex align-items-center justify-content-center">
+            <div className="p-2 bg-light d-flex align-items-center justify-content-center" style={{borderRadius: "50%"}}>
               <div className="chikn-img d-flex align-items-center justify-content-center">
                 <img src={chicken.imgurl} />
               </div>
             </div>
-            <div style={{ height: "220px" }}>
+            <div style={{ height: "220px", backgroundColor: 'rgba(0, 0, 0, 0.5)', color: "white"}}>
               <h2 className="mt-3 d-flex align-items-baseline">
                 {chicken.name}
-                <small className="text-sm text-muted ms-3 d-flex align-items-center">
+                <small className="text-sm ms-3 d-flex align-items-center">
                   <FaMapMarkerAlt style={{ width: 20 }} className="me-2" />
                   {chicken.location}
                 </small>
@@ -75,7 +70,6 @@ function RateChickens() {
             </div>
             <div className="d-flex justify-content-between px-2 py-4">
               <Button
-                outline
                 color="primary"
                 size="lg"
                 className="shadow-sm"
@@ -84,7 +78,6 @@ function RateChickens() {
                 🚫 Cool Ranch 🥶
               </Button>
               <Button
-                outline
                 color="danger"
                 size="lg"
                 className="shadow-sm"
