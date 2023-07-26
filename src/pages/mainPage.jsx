339acb8aa305ee390f1e17551f7db0d86b2469dc
Jonathan Maimone <jonathanfmaimone@gmail.com>
2023-07-26 16:36:06 -0400

    header switch mainpage

--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -18,21 +18,21 @@ function Carouseler(args) {
   
     const items = [
       {
-        onClick: () => navigate("/View"),
+        onClick: () => navigate("/view"),
         src: "https://wikifarmer.com/wp-content/uploads/2017/06/Chicken-Coop-%E2%80%93-Hen-House.jpg",
         altText: "View All Chick'nz",
         caption: "View All Chick'nz",
         key: 1,
       },
       {
-        onClick: () => navigate("/Submit"),
+        onClick: () => navigate("/submit"),
         src: "https://picsum.photos/id/456/1200/400",
         altText: "Submit Chick'n",
         caption: "Submit Chick'n",
         key: 2,
       },
       {
-        onClick: () => navigate("/Rate"),
+        onClick: () => navigate("/rate"),
         src: "https://picsum.photos/id/678/1200/400",
         altText: "Rate your Chick'n",
         caption: "Rate your Chick'n",
@@ -74,7 +74,6 @@ function Carouseler(args) {
   
     return (
       <>
-      <div><Header /></div>
       <Carousel
         activeIndex={activeIndex}
         next={next}
@@ -98,6 +97,7 @@ function Carouseler(args) {
           onClickHandler={next}
         />
       </Carousel>
+      <Header />
     </>
     );
   }
