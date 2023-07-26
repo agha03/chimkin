561c7b3bb842eae70dc2cb78ce2689b681aa0ae3
agha03 <moe.agha03@gmail.com>
2023-07-26 15:49:54 -0400

    fixed view

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import RateChickens from "./pages/RateChickens";
 import SubmitChicken from "./pages/SubmitChicken";
 import ViewChickens from "./pages/ViewChickens";
 import ToastNotify from "./components/Toast";
+import MainPage from "./components/mainPage"
 
 
 function App() {
   return (
 	<div>
     	<Routes>
-      		<Route path="/" element={<RateChickens />} />
+      		<Route path="/rate" element={<RateChickens />} />
       		<Route path="/submit" element={<SubmitChicken />} />
       		<Route path="/view" element={<ViewChickens />} />
-			
+			<Route path ="/" element ={<MainPage />} />
     		</Routes>
     		<ToastNotify />
     	</div>
